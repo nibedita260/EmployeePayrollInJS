b5e74bbc811bbfe61235ba7da78140b82f911e89
nibedita260 <deepi.nibedita260@gmail.com>
2021-12-22 17:08:47 +0530

    [Nibedita]Added:Refactored empRegister page in EmployeePayroll Features Implemented

--- js/employee.js
+++ js/employee.js
@@ -1,16 +1,37 @@
 
 
 function ClearFields() {
-    document.getElementById("name").value = "";
-    document.getElementById("empImg").value = "";
-    document.querySelector('input[type=checkbox][name=gender]:checked').checked = false;
+
+    var name=document.getElementById("name") ;
+    if(name.value!=null){
+        document.getElementById("name").value="";
+    }
+    // var empImg=document.getElementById("empImg").value;
+    // if(empImg.value!=null){
+    //     document.getElementById("empImg").value="";
+    // }
+    var gender=document.querySelector("input[type=radio][name=gender]:checked");
+    if(gender.value=='M' || gender.value=='F'){
+        document.querySelector("input[type=radio][name=gender]:checked").checked = false;
+    }
     var checkboxes = document.getElementsByName('department');
     for (var checkbox of checkboxes) {
-        checkbox.checked = false;
+        if(checkbox.checked==true){
+            checkbox.checked = false;
+        }
     }
-    document.getElementById("salary").value = "";
-    document.getElementById("startdate").value = "";
-    document.getElementById("notes").value = "";
+    var salary=document.getElementById("salary").value;
+    if(salary!=null){
+        document.getElementById("salary").value="";
+    }
+    var startdate=document.getElementById("startdate").value ;
+    if(startdate!=null){
+        document.getElementById("startdate").value = "";
+    }
+    var notes=document.getElementById("notes").value ;
+    if(notes!=null){
+        document.getElementById("notes").value = "";
+    }
 }
 
 function submitForm() {
@@ -26,11 +47,11 @@ function submitForm() {
     }
     
     var name=document.getElementById("name").value;
-    var genderCheck=document.querySelector('input[type=checkbox][name=gender]:checked');
-    var departmentCheck=document.querySelectorAll('input[type=checkbox][name=department]:checked').length;
+    var genderCheck=document.querySelector("input[type=radio][name=gender]:checked");
+    var departmentCheck=document.querySelectorAll("input[type=checkbox][name=department]:checked").length;
     var salary=document.getElementById("salary").value;
     var startdate=document.getElementById("startdate").value ;
-    //var notes=document.getElementById(notes).value;
+    var notes=document.getElementById("notes").value;
 
     //validate
     if (name == "") {
@@ -57,20 +78,38 @@ function submitForm() {
         error=salaryErr;
         return false;
     }
-    if (startdate == null) {
+    if (startdate == '') {
         window.alert("Please Fill startdate Field.");
         salaryErr="Please Fill startdate Field.";
         error=startdateErr;
         return false;
     }
-    if(notes==null){
+    var todaysDate = new Date();
+
+    function convertDate(date) {
+      var yyyy = date.getFullYear().toString();
+      var mm = (date.getMonth()+1).toString();
+      var dd  = date.getDate().toString();
+    
+      var mmChars = mm.split('');
+      var ddChars = dd.split('');
+    
+      return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
+    }
+    
+    console.log(convertDate(todaysDate));
+    if (startdate<convertDate(todaysDate)) {
+        window.alert("The Date must be Bigger or Equal to today date")
+        return false;
+    }
+    if(notes==''){
         window.alert("Please Fill notes Field.");
         salaryErr="Please Fill notes Field.";
         error=notesErr;
         return false;
     }
     console.log(error);
-    var gender=document.querySelector('input[type=checkbox][name=gender]:checked').value;
+    var gender=document.querySelector('input[type=radio][name=gender]:checked').value;
     //submit input
     var empData=`name is ${name} gender is ${gender} ,notes ${notes} ,department ${department},salary ${salary},startdate ${startdate}`;
     
